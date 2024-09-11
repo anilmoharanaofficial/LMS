2ccf898e0a198d508f71b3a5db11462b1da5cae0
Anil Moharana <anil.k.moharana@gmail.com>
2024-09-11 15:16:32 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,43 +1,13 @@
+import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
-import erroMiddleware from "./middlewares/errorMiddleware.js";
-import courceRoutes from "./routes/courseRoutes.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
 
+config();
 const app = express();
-
-//Middleware.....
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
-  })
-);
 
-//Routes.........
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/cources", courceRoutes);
-app.use("/api/v1/payment", paymentRoutes);
-
-//Testig......
-app.use("/ping", (req, res) => {
-  res.send("Server is Running.......");
-});
-
-//Non-existent URL..........
-app.all("*", (req, res) => {
-  res.status(404).send("OOPS!! 404 Page Not Found.....");
-});
-
-//Error Middlewre
-app.use(erroMiddleware);
-
-//Exportig.........
 export default app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,7 @@
 import app from "./app.js";
-import connectToDB from "./config/database.js";
-import cloudinary from "cloudinary";
-import { config } from "dotenv";
-config();
-import Razorpay from "razorpay";
 
-const PORT = process.env.PORT || 4546;
-
-//Cloudinary configuration
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-//Razorpay
-export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET,
-});
+const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {
-  //DB Connection
-  await connectToDB();
-  console.log(`Server is listening at http://localhost:${PORT}`);
+  console.log(`SERVER IS RUNNING AT PORT:${PORT}`);
 });
