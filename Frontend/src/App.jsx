93657b3431950a2c1eb5456e293cbe9dc800e66f
Anil Moharana <anil.k.moharana@gmail.com>
2024-09-15 21:17:48 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>yogisteye</div>
+    </>
+  );
+}
+
+export default App;
